52d0e9bd46cf777d5cb6d94861f192e190fc0a1a
danielafervenefwdcostarica-droid <danielafervenefwdcostarica@gmail.com>
2026-07-01 10:05:19 -0600

    extraer cambios

--- debug.js
+++ debug.js
@@ -6,7 +6,7 @@ envFile.split('\n').forEach(line => {
   if (match) env[match[1].trim()] = match[2].trim();
 });
 
-fetch(`${env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/estudiantes?select=user_id,busca_financiamiento,proyecto_titulo&limit=5`, {
+fetch(`${env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/curriculums?select=*&limit=1`, {
   headers: {
     'apikey': env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     'Authorization': `Bearer ${env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`
--- next-env.d.ts
+++ next-env.d.ts
@@ -1,9 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-<<<<<<< HEAD
-=======
-import "./.next/types/routes.d.ts";
->>>>>>> 6e2ffbd6979c7a4a65ed780253071bfc30e4e425
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
